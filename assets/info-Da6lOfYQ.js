923523fe430954a3fd8c6379177e3757911c0fd0
yookoala <yookoala@users.noreply.github.com>
2026-06-15 13:18:27 +0000

    deploy: 9450d58a3ba13a1153add1fec31161d3836071c6

--- assets/info-Da6lOfYQ.js
+++ assets/info-Da6lOfYQ.js
@@ -8,9 +8,4 @@ import{c as e}from"./createLucideIcon-A35IBt24.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const a=e("Database",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]]);/**
- * @license lucide-react v0.378.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const l=e("Info",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 16v-4",key:"1dtifu"}],["path",{d:"M12 8h.01",key:"e9boi3"}]]);export{y as C,a as D,l as I};
+ */const r=e("Info",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 16v-4",key:"1dtifu"}],["path",{d:"M12 8h.01",key:"e9boi3"}]]);export{y as C,r as I};
